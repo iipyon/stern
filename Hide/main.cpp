8311bb3fc1d1163fdaa041e871f592592a54e7c5
MM <Ia>
2019-02-25 17:14:14 +0900

    dghb

---
+++ Hide/main.cpp
@@ -0,0 +1,26 @@
+#include "DxLib.h"
+
+// WinMain関数
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	// 画面モードの設定
+	SetGraphMode(600, 600, 32);//ウィンドウのサイズを決める
+	ChangeWindowMode(TRUE);// ウィンドウモード変更
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1) { return -1; };
+
+	//-------------------------------------------------
+	SetDrawScreen(DX_SCREEN_BACK);//裏画面設定
+
+	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {//画面更新＆メッセージ処理&画面殺害
+		
+
+
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;					// ソフトの終了
+}
