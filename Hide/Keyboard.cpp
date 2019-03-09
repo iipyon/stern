fe6836fa12f28ec8b31ec7c839848161d3bcefef
Tatsuki <contactme0522@gmail.com>
2019-03-09 09:32:47 +0900

    キーボードの問題を修正

--- Hide/Keyboard.cpp
+++ Hide/Keyboard.cpp
@@ -1,26 +1,39 @@
 #include "Keyboard.h"
 
+Keyboard::Keyboard()
+{
+	current = new char[KEY_BOARD_BUF_SIZE];
+	previous = new char[KEY_BOARD_BUF_SIZE];
+}
+
+Keyboard::~Keyboard()
+{
+	delete[] current;
+	delete[] previous;
+}
+
 void Keyboard::update()
 {
-	*previous=*current;
-	char Buf[256];
-	GetHitKeyStateAll(Buf);
-	*current = *Buf;
-	
+	// —Ìˆæ‚ð“ü‚ê‘Ö‚¦‚é
+	work = current;
+	current = previous;
+	previous = work;
+
+	GetHitKeyStateAll(current);	
 }
 
 bool Keyboard::key_down(int k_)
 {
 
-	return current[k_] == 1&& previous[k_] == 0;
+	return *(current+k_) == 1 && *(previous+k_) == 0;
 }
 
 bool Keyboard::key_up(int k_)
 {
-	return current[k_] == 0&& previous[k_]==1;
+	return *(current+k_) == 0&& *(previous+k_)==1;
 }
 
 bool Keyboard::key_press(int k_)
 {
-	return current[k_]==1;
+	return *(current + k_)==1;
 }
--- Hide/Keyboard.h
+++ Hide/Keyboard.h
@@ -1,18 +1,21 @@
 #pragma once
 #include"DxLib.h"
+#include <memory>
+#define KEY_BOARD_BUF_SIZE 256
+
 
 class Keyboard {
 public:
-	Keyboard() {
-		
-	}
-	//メソッド
+	Keyboard();
+	~Keyboard();
 	void update();
 	bool key_down(int);
 	bool key_up(int);
 	bool key_press(int);
 	//bool Get(KEY_INPUT *key);
 private:
-	int current[256];//キーボード対応させる
-	int previous[256];//１フレーム前の配列を記憶
+	//DXライブラリの仕様上スマートポインタにできない
+	char* current;	//キーボード対応させる
+	char* previous;	//１フレーム前の配列を記憶
+	char* work;	//作業領域
 };
