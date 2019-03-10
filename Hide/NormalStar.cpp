cc55e5d4270bae29f138b14b476ac066e66b2c63
MM <Ia>
2019-03-10 17:46:14 +0900

    Add velocityX,Y

--- Hide/NormalStar.cpp
+++ Hide/NormalStar.cpp
@@ -9,8 +9,8 @@ void NormalStar::update()
 {
 	DrawFormatString(300, 0, GetColor(255, 0, 0), "%d", power);
 	//‰¼‚ÌˆÚ“®
-	y += (int)velocity;
-	x += (int)velocity;
+	y += (int)velocityY;
+	x += (int)velocityX;
 
 	draw();
 }
