235ca00fef1bc30df815888af64137bd98874142
iipyon <kyota99930@yahoo.co.jp>
2019-03-13 23:55:27 +0900

    MapUpDate

--- Hide/Map.cpp
+++ Hide/Map.cpp
@@ -8,7 +8,8 @@
 
 Map::Map(char *mapfp)
 {
-	//1津のチップの大きさを30と考える(勝手)
+	//1津のチップの大きさを30と考える(勝手)//チップサイズは32が妥当
+	chipsize = 30;
 	mapsizex = 600;
 	mapsizey = 600;
 	//使う画像決定
@@ -16,8 +17,8 @@ Map::Map(char *mapfp)
 	//ファイル読み込み
 	std::ifstream mapdata(mapfp);
 	if (!mapdata) { return; }
-	for (int y = 0; y < mapsizey / 30; ++y) {
-		for (int x = 0; x < mapsizex / 30; ++x) {
+	for (int y = 0; y < mapsizey / chipsize; ++y) {
+		for (int x = 0; x < mapsizex / chipsize; ++x) {
 			mapdata >> data[y][x];
 		}
 	}
@@ -27,9 +28,9 @@ Map::Map(char *mapfp)
 
 void Map::draw()
 {
-	for (int y = 0; y < mapsizey / 20; ++y) {
-		for (int x = 0; x < mapsizex / 20; ++x) {
-			DrawRectGraph(x * 30, y * 30, data[y][x] * 30, 0, 30, 30, graph, FALSE);
+	for (int y = 0; y < mapsizey / (mapsizey/chipsize); ++y) {
+		for (int x = 0; x < mapsizex / (mapsizex / chipsize); ++x) {
+			DrawRectGraph(x * chipsize, y *chipsize, data[y][x] * chipsize, 0, chipsize, chipsize, graph, FALSE);
 		}
 	}
 }
@@ -38,13 +39,15 @@ void Map::update()
 {
 	draw();
 }
-
+//-----------------------------------------------------------------------------------------------------------
+//問題点　charaが二つのマップチップをまたいでいるとき左（または上)のチップのみリターンされる
+//-----------------------------------------------------------------------------------------------------------
 int Map::get_left(Point chara_)
 {
-	int sx = chara_.x / 30;
-	int sy = chara_.y / 30;
-	int ex = (chara_.x + chara_.w) / 30;
-	int ey = (chara_.y + chara_.h) / 30;
+	int sx = (chara_.x-1 )/ chipsize;
+	int sy = chara_.y / chipsize;
+	int ex = chara_.x / chipsize;
+	int ey = (chara_.y + chara_.h) / chipsize;
 	for (int y = sy; y < ey; ++y) {
 		for (int x = sx; sx < ex; ++x) {
 			return data[y][x];
@@ -53,23 +56,41 @@ int Map::get_left(Point chara_)
 	return 0;
 }
 
-int Map::get_right(Point)
+int Map::get_right(Point chara_)
 {
+	int sx = (chara_.x + chara_.w) / chipsize;
+	int sy = chara_.y / chipsize;
+	int ex = ((chara_.x + chara_.w) +1)/ chipsize;
+	int ey = (chara_.y + chara_.h) / chipsize;
+	for (int y = sy; y < ey; ++y) {
+		for (int x = sx; sx < ex; ++x) {
+			return data[y][x];
+		}
+	}
 	return 0;
 }
 
-int Map::get_top(Point)
+int Map::get_top(Point chara_)
 {
+	int sx = chara_.x/ chipsize;
+	int sy = (chara_.y-1) / chipsize;
+	int ex = (chara_.x + chara_.w) / chipsize;
+	int ey = chara_.y / chipsize;
+	for (int y = sy; y < ey; ++y) {
+		for (int x = sx; sx < ex; ++x) {
+			return data[y][x];
+		}
+	}
 	return 0;
 }
 
 int Map::get_bottom(Point chara_)
 {
 	//マップタイル１つが30*30のため
-	int sx = chara_.x / 30;
-	int sy = chara_.y / 30;
-	int ex = (chara_.x + chara_.w) / 30;
-	int ey = (chara_.y + chara_.h) / 30;
+	int sx = chara_.x / chipsize;
+	int sy = (chara_.y + chara_.h) / chipsize;
+	int ex = (chara_.x + chara_.w) / chipsize;
+	int ey = ((chara_.y + chara_.h)+1) / chipsize;
 	//範囲内の障害物を探す
 	for (int y = sy; y <= ey; ++y) {
 		for (int x = sx; sx <= ex; ++x) {
