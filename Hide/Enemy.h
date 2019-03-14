8fa75b8fc0f41f447c41cc47e3050ff4b50d4fa0
tonegawa <1>
2019-03-14 23:58:35 +0900

    Add6Enemyclass
    スペルミス訂正

--- Hide/Enemy.h
+++ Hide/Enemy.h
@@ -13,11 +13,7 @@ public :
 	//メソッド
 	//コンストラクタ
 	Enemy();
-<<<<<<< HEAD
 	Enemy(int E_life, int E_damaged, int E_gravity);
-=======
-	Enemy(int E_life, int E_damage, int E_gravity);
->>>>>>> Enemyclass
 	virtual void move();
 	bool attack();
 	void update();
