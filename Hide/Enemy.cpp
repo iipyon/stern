4948d02f4d5d410c5ba5119b7af1c0a7be19a708
tonegawa <1>
2019-03-14 23:26:23 +0900

    Add6Enemyclass
    コンストラクタの変更

--- Hide/Enemy.cpp
+++ Hide/Enemy.cpp
@@ -2,11 +2,17 @@
 //----------------------------------
 //“G‘S”Ê
 //----------------------------------
-Enemy::Enemy(int life, int damage, int gravity)
+Enemy::Enemy()
 {
 	life = 0;
-	damage = 0;
+	damaged = 0;
 	gravity = 0;
+}
+Enemy::Enemy(int E_life, int E_damaged, int E_gravity)
+{
+	life = E_life;
+	damaged = E_damaged;
+	gravity = E_gravity;
 }
 void Enemy::move()
 {
