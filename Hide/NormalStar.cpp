457aef0cbefa0ea54fe9e87b278df46f1847dc8e
iipyon <kyota99930@yahoo.co.jp>
2019-03-18 22:31:06 +0900

    星に関するアップデート スターマネージャーのせいで一瞬しか表示できないためデバックできない

--- Hide/NormalStar.cpp
+++ Hide/NormalStar.cpp
@@ -1,7 +1,8 @@
 #include"NormalStar.h"
 #include"DxLib.h"
 #include<math.h>
-#include"GameTaskSystem.h"
+#include"CoreTask.h"
+
 
 //--------------------------------
 //普通の星
@@ -10,14 +11,34 @@
 void NormalStar::update()
 {
 	DrawFormatString(300, 0, GetColor(255, 0, 0), "%d", power);
-	//仮の移動
-	x += (int)(-sin(angle) * velocityX);
-	y += (int)(cos(angle) * velocityY);
+	//仮の移動//move（）作らね？
+	if (contact) {
+		x += (int)(-sin(angle) * velocityX);
+		y += (int)(cos(angle) * velocityY);
+		if (ct->gts->map->get_bottom&&ct->gts->map->get_top&&ct->gts->map->get_left&&ct->gts->map->get_right) {
+			contact = true;
+
+		}
+	}
+	else {
+		this->exercise();
+	}
+	if (attack()) {
+		//enemyのダメージを呼ぶ
+	}
 
 	draw();
+	if (damage(1)) {
+		ct->gts->normalstar->destroy();
+	}
+	//if(gravitystarがあるかどうか)｛//重力がかかる範囲を決めて近くになかったらfalseでもいいかも
+	//gravitystarの座標を取り続ける
+	//inhale(gravitystarの座標);
+	//｝
 }
 
 void NormalStar::inhale(int, int, float)
 {
+	//移動量にgravitystarの座標との差を出して足し続ける
 }
 
--- Hide/Star.cpp
+++ Hide/Star.cpp
@@ -11,15 +11,22 @@ Star::Star(int power_, int life_, int weight_,int x_,double angle_)
 	power = power_;
 	weight = weight_;
 	life = life_;
+	contact = false;
+	bright = 0;//¡‚Ì‚Æ‚±‚ë
 }
 
 bool Star::attack()
 {
+	//‰~‚Æ‹éŒ`‚Ì“–‚½‚è”»’è
 	return false;
 }
 
-bool Star::damage(int)
+bool Star::damage(int damage_)
 {
+	life -= damage_;
+	if (life <= 0) {
+		return true;
+	}
 	return false;
 }
 
