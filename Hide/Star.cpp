be8ecc19f57d1ea2c36d312b92d1d9ad39acbbd4
MM <Ia>
2019-03-26 14:02:11 +0900

    StarStateの記述場所修正

--- Hide/Star.cpp
+++ Hide/Star.cpp
@@ -6,7 +6,11 @@
 
 Star::Star(Point point_, PhysicState physic_state_, StarState star_state) : Physic(point_, physic_state_)
 {
-
+	bright = star_state.bright;
+	radius = star_state.radius;
+	power = star_state.power;
+	life = star_state.life;
+	angle = star_state.angle;
 }
 
 bool Star::attack()
