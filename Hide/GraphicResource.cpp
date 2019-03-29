f526275b16abf74ecf70122a4c3da7dc1e6d8819
Tatsuki <contactme0522@gmail.com>
2019-03-29 13:20:38 +0900

    例外を処理できるようにruntime_errorに変更

--- Hide/GraphicResource.cpp
+++ Hide/GraphicResource.cpp
@@ -11,7 +11,7 @@ GraphicResource::GraphicResource()
 	std::ifstream ifs("img/resource.json");
 	if (ifs.fail())
 	{
-		throw "resource.json is not found.";	//ファイルが読み込めないと例外を返す
+		throw std::runtime_error("resource.json is not found.");	//ファイルが読み込めないと例外を返す
 	}
 	std::istreambuf_iterator<char> it(ifs);
 	std::istreambuf_iterator<char> last;
@@ -24,28 +24,6 @@ GraphicResource::GraphicResource()
 	}
 	graph = std::make_unique<GraphicObject[]>(count_of_graph);	//画像の枚数分の領域を確保する
 	for (int i = 0; i < count_of_graph; i++) graph[i].exist = false;
-
-//	//下記の処理はload()へ移動予定  <- 済み
-
-//	int i = 0;
-//	for (auto &item : json["graph"].array_items()) {
-//		graph[i].handle = new int[count_of_graph];	//アニメーション画像のフレーム枚数分のハンドル領域を確保する
-//		LoadDivGraph(
-//			item["path"].string_value().c_str(),
-//			item["column"].int_value() * item["line"].int_value(),
-//			item["column"].int_value(),
-//			item["line"].int_value(), 
-//			item["width"].int_value(), 
-//			item["height"].int_value(),
-//			graph[i].handle
-//		);		//JSONに書かれた情報をLoadDivGraphから読み込む
-//		graph[i].name = item["name"].string_value();	//JSONで指定された"name"はget()の引数に文字列として指定が可能
-//		graph[i].max = item["line"].int_value()*item["column"].int_value();
-////		slide = item["max"].number_value();
-//		i++;
-//	}
-	//load("player");	//試験用
-
 }
 //デストラクタ
 GraphicResource::~GraphicResource()
