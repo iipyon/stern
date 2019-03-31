2a7b1c9a947a3bd7663a98e20cb5b2b4ac8cfd8c
MM <Ia>
2019-03-31 17:08:21 +0900

    飛行敵の挙動改善、生成

--- Hide/FlyingEnemy.h
+++ Hide/FlyingEnemy.h
@@ -9,15 +9,18 @@ class FlyingEnemy : public Enemy{
 public:
 	enum class FlyingState
 	{
-		fly,
+		up,
+		down,
 		stay
 	};
 	FlyingState flyingstate;
 
 	FlyingEnemy(Point point_, PhysicState physic_state_, EnemyState enemy_state_);
 	void move();
+	void change_state();
 	void update();
 private:
-	int FlyingEnemycnt = 0;
+	int preY;//移動前をとる
+	int destinationY;//移動先をとる
 protected:
 };
