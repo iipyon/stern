f51def5aa1d0c9e89d29462d288301d6f6bc810c
tonegawa <1>
2019-03-31 10:42:41 +0900

    FlyingEnemyの更新

--- Hide/FlyingEnemy.cpp
+++ Hide/FlyingEnemy.cpp
@@ -1,4 +1,5 @@
 ﻿#include "FlyingEnemy.h"
+#include "CoreTask.h"
 
 //----------------------------------
 //雑魚敵(飛行)
@@ -6,18 +7,23 @@
 
 FlyingEnemy::FlyingEnemy(Point point_, PhysicState physic_state_, EnemyState enemy_state_) : Enemy(point_, physic_state_, enemy_state_)
 {
+
+	//必要か判断ができない初期化処理
+	flyingstate = FlyingState::fly;
+	init_render("flying");
 }
 
 void FlyingEnemy::move()
 {
-	switch(flyingstate)
+	switch (flyingstate)
 	{
+	//飛行
 	case FlyingState::fly:
 		//2マス分
 		if (FlyingEnemycnt < 60) {
 			velocityY = 1;
 		}
-		else{
+		else {
 			velocityY = -1;
 		}
 		break;
@@ -27,14 +33,20 @@ void FlyingEnemy::move()
 		FlyingEnemycnt--;
 		break;
 	}
-}
+	point.y += (int)velocityY;
 
-void FlyingEnemy::update()
-{
 	//FlyingEnemyをカウントする
 	FlyingEnemycnt++;
 	//120以上になったらリセット
 	if (FlyingEnemycnt > 120) {
 		FlyingEnemycnt = 0;
 	}
+}
+
+void FlyingEnemy::update()
+{
+	exercise();
+	move();
+	DrawFormatString(400, 0, GetColor(0, 0, 0), "%d", velocityY);
+	draw(true);
 }
--- Hide/FlyingEnemy.h
+++ Hide/FlyingEnemy.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+﻿ #pragma once
 #include"Enemy.h"
 
 //---------------------------------
@@ -13,8 +13,8 @@ public:
 		stay
 	};
 	FlyingState flyingstate;
+
 	FlyingEnemy(Point point_, PhysicState physic_state_, EnemyState enemy_state_);
-	//NormalEnemy* list;//リスト構造のため一応記述
 	void move();
 	void update();
 private:
